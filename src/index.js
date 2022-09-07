30d3bf77af191590ce2e94e7b199ab042474b087
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-07 18:40:12 +0100

    add app.js to contain app js files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Calculator from './components/calculator';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="calculator-container">
-      <Calculator />
+    <div>
+      <App />
     </div>
   </React.StrictMode>,
 );
