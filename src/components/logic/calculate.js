07913c179d9163aaa09fec77b3bb203f91f25149
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-09 11:36:49 +0100

    Refactor states and class to functions

--- src/components/logic/calculate.js
+++ src/components/logic/calculate.js
@@ -87,14 +87,6 @@ export default function calculate(obj, buttonName) {
     return {};
   }
 
-  // Button must be an operation
-
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
-
   // User pressed an operation after pressing '='
   if (!obj.next && obj.total && !obj.operation) {
     return { ...obj, operation: buttonName };
