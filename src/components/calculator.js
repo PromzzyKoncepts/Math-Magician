d63123c19afa0e4ae757bad67031d3e3f48e9c16
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-09 11:34:31 +0100

    Refactor states and class to functions

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,35 +1,31 @@
 // import { render } from '@testing-library/react';
-
-import React from 'react';
+import React, { useState } from 'react';
 import DisplayScreen from './displayScreen';
 import NumericKeys from './numericKeys';
 import OperatorKeys from './operatorkeys';
 import calculate from './logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const Calculator = () => {
+  const [state, setState] = useState({
+    obj: {
       total: null,
       next: null,
       operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+    },
+  });
 
-    handleClick = (event) => {
-      this.setState((state) => calculate(state, event.target.textContent));
-    };
-
-    render() {
-      const { total, next, operation } = this.state;
-      return (
-        <div className="calc-container">
-          <DisplayScreen next={next} total={total} operation={operation} />
-          <OperatorKeys handleClick={this.handleClick} />
-          <NumericKeys handleClick={this.handleClick} />
-        </div>
-      );
-    }
-}
+  const handleClick = (event) => {
+    const { obj } = state;
+    setState({ obj: calculate(obj, event.target.textContent) });
+  };
+  const { obj } = state;
+  const { total, next, operation } = obj;
+  return (
+    <div className="calc-container">
+      <DisplayScreen next={next} total={total} operation={operation} />
+      <OperatorKeys handleClick={handleClick} />
+      <NumericKeys handleClick={handleClick} />
+    </div>
+  );
+};
 export default Calculator;
