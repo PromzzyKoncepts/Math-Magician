bdf6d0d74fdfe00495dc2127f4101bebe98d15df
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-09 11:35:59 +0100

    Refactor states and class to functions

--- src/components/operatorkeys.js
+++ src/components/operatorkeys.js
@@ -1,25 +1,19 @@
-/*eslint-disable*/
-import { Component } from 'react';
+// /*eslint-disable*/
+import React from 'react';
 
-class OperatorKeys extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { handleClick } = this.props;
-    return (
-      <>
-        <button type="button" className="btn btn-top" onClick={handleClick}>AC</button>
-        <button type="button" className="btn btn-top" onClick={handleClick}>+/-</button>
-        <button type="button" className="btn btn-top" onClick={handleClick}>%</button>
-        <button type="button" className="btn btn-side" onClick={handleClick}>÷</button>
-        <button type="button" className="btn btn-side" onClick={handleClick}>x</button>
-        <button type="button" className="btn btn-side" onClick={handleClick}>-</button>
-        <button type="button" className="btn btn-side" onClick={handleClick}>+</button>
-        <button type="button" className="btn btn-side" onClick={handleClick}>=</button>
-      </>
-    );
-  }
-}
+const OperatorKeys = (props) => {
+  const { handleClick } = props;
+  return (
+    <>
+      <button type="button" className="btn btn-top" onClick={handleClick}>AC</button>
+      <button type="button" className="btn btn-top" onClick={handleClick}>+/-</button>
+      <button type="button" className="btn btn-top" onClick={handleClick}>%</button>
+      <button type="button" className="btn btn-side" onClick={handleClick}>÷</button>
+      <button type="button" className="btn btn-side" onClick={handleClick}>x</button>
+      <button type="button" className="btn btn-side" onClick={handleClick}>-</button>
+      <button type="button" className="btn btn-side" onClick={handleClick}>+</button>
+      <button type="button" className="btn btn-side" onClick={handleClick}>=</button>
+    </>
+  );
+};
 export default OperatorKeys;
