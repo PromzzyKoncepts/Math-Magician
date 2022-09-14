875697846b4be3ec477d58902eeac0ce89e5345f
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-14 13:50:39 +0100

    Add inline stylings for file

--- src/Pages/Quotes.js
+++ src/Pages/Quotes.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
+const quoteStyle = {
+  width: '70%',
+  margin: 'auto',
+  transform: 'translateY(120px)',
+  textAlign: 'justify',
+};
+
 const Quote = () => (
   <>
-    <main>
-      <header className="Quote-heading">
-        <h2>Inspiring Mathematical Quotes</h2>
-      </header>
+    <main style={quoteStyle}>
       <p>
         Mathematics are the result of mysterious powers
         which no one understands, and which the unconscious
         recognition of beauty must play an important part.
         Out of an infinity of designs a mathematician chooses
         one pattern for beauty’s sake and pulls it down to earth.
-        <span>
+        <br />
+        <span style={{ fontWeight: '600', float: 'right' }}>
           — Marston Morse,
-          <small>American mathematician</small>
+          <small style={{ color: 'brown' }}> American mathematician</small>
         </span>
       </p>
     </main>
