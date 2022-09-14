50c9230388adfe489d31b9e3e6c78ae94031e24c
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-14 12:42:12 +0100

    Update file to include routers and routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './Pages/Home';
+import Quote from './Pages/Quotes';
+import NavBar from './Pages/NavBar';
 
 const App = () => (
-  <div className="calculator-container">
-    <Calculator />
-  </div>
+  <>
+    <NavBar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/calculator" exact element={<Calculator />} />
+      <Route path="/quote" exact element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
