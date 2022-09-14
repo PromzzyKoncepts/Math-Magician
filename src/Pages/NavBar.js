4e678b86ebfaab8968e13f752b814214b5f0a616
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-14 13:50:54 +0100

    Add inline stylings for file

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const HeaderStyle = {
+  color: 'bisque',
+};
+
 const NavBar = () => (
   <>
     <nav className="nav-container">
-      <h1>Math Magicians</h1>
+      <h1 style={HeaderStyle}>
+        Math
+        <span style={{ color: '#f5913e' }}>Magicians</span>
+      </h1>
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/quote">Quote</Link></li>
+        <li className="link"><Link to="/">Home</Link></li>
+        <li className="link"><Link to="/calculator">Calculator</Link></li>
+        <li className="link"><Link to="/quote">Quote</Link></li>
       </ul>
     </nav>
   </>
 );
-
-// const Link = ({ to, children }) => {
-//   const isActive = useMatch({ path: useResolvedPath(to).pathname, end: true });
-
-//   return (
-//     <li>
-//       <Link className={isActive ? 'active' : ''} to={to}>{children}</Link>
-//     </li>
-//   );
-// };
 
 export default NavBar;
