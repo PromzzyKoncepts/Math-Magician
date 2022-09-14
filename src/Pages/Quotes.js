9800ba6a2a0aa93f45e7f9ed6ce04c9e0dc813e8
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-14 12:41:36 +0100

    Add Quote file for routing pages

---
+++ src/Pages/Quotes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Quote = () => (
+  <>
+    <main>
+      <header className="Quote-heading">
+        <h2>Inspiring Mathematical Quotes</h2>
+      </header>
+      <p>
+        Mathematics are the result of mysterious powers
+        which no one understands, and which the unconscious
+        recognition of beauty must play an important part.
+        Out of an infinity of designs a mathematician chooses
+        one pattern for beauty’s sake and pulls it down to earth.
+        <span>
+          — Marston Morse,
+          <small>American mathematician</small>
+        </span>
+      </p>
+    </main>
+  </>
+);
+export default Quote;
