b8074a1fe88c380bdcb26da8c7f1662dc38b2b94
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-15 10:13:00 +0100

    Write test for Calculator.js component

---
+++ src/__test__/calculator.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../components/calculator';
+
+it('renders Calculator correctly', () => {
+  const tree = renderer.create(<Calculator />)
+  expect(tree).toMatchSnapshot()
+});
